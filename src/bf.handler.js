adab0bb7b7039571215d4480537c0ed599a22436
Imperialgfx <impserv178@gmail.com>
2024-12-03 15:08:36 -0800

    Update bf.handler.js
    upd ultraviolet DOM handling with API's

--- src/bf.handler.js
+++ src/bf.handler.js
@@ -1,52 +1,65 @@
-const BlissflyHandler = {
-    config: {
-        prefix: '/service/',
-        handler: '/bf.handler.js',
-        bundle: '/bf.bundle.js',
-        config: '/bf.config.js',
-        sw: '/bf.sw.js'
-    },
+class BlissFlyClient extends EventEmitter {
+    constructor(window = self, bareClient, worker = !window.window) {
+        super();
+        this.window = window;
+        this.nativeMethods = {
+            fnToString: this.window.Function.prototype.toString,
+            defineProperty: this.window.Object.defineProperty,
+            getOwnPropertyDescriptor: this.window.Object.getOwnPropertyDescriptor,
+            getOwnPropertyNames: this.window.Object.getOwnPropertyNames,
+            keys: this.window.Object.keys,
+            getOwnPropertySymbols: this.window.Object.getOwnPropertySymbols,
+            isArray: this.window.Array.isArray,
+            setPrototypeOf: this.window.Object.setPrototypeOf,
+            isExtensible: this.window.Object.isExtensible,
+            Map: this.window.Map
+        };
+        this.worker = worker;
+        this.bareClient = bareClient;
+        this.location = new LocationApi(this);
+        this.document = new DocumentApi(this);
+        this.element = new ElementApi(this);
+        this.style = new StyleApi(this);
+    }
 
     rewriteUrl(url) {
-        return this.config.prefix + btoa(encodeURIComponent(url));
-    },
-
-    transformResponse(response, url) {
-        const contentType = response.headers.get('content-type') || '';
-        
-        if (contentType.includes('html')) {
-            return this.transformHtml(response, url);
-        } else if (contentType.includes('javascript')) {
-            return this.transformJs(response);
-        } else if (contentType.includes('css')) {
-            return this.transformCss(response, url);
-        }
-        
-        return response;
-    },
-
-    async transformHtml(response, baseUrl) {
-        const html = await response.text();
-        const injectedScript = `
-            <script>
-                window.__blissfly = {
-                    baseUrl: "${baseUrl}",
-                    prefix: "${this.config.prefix}"
-                };
-                
-                // Intercept fetch requests
-                const originalFetch = window.fetch;
-                window.fetch = async function(url, options) {
-                    if (url && !url.startsWith('data:')) {
-                        url = '/service/' + btoa(encodeURIComponent(url));
-                    }
-                    return originalFetch(url, options);
-                };
-            </script>
-        `;
-        
-        return html.replace('</head>', `${injectedScript}</head>`);
+        return this.prefix + this.codec.encode(url);
     }
-};
 
-module.exports = BlissflyHandler;
+    sourceUrl(url) {
+        return this.codec.decode(url.slice(this.prefix.length));
+    }
+}
+
+class DocumentApi {
+    constructor(bf) {
+        this.bf = bf;
+        this.window = bf.window;
+        this.Document = this.window.Document;
+        this.docProto = this.Document.prototype;
+        this.title = bf.nativeMethods.getOwnPropertyDescriptor(this.docProto, "title");
+        this.referrer = bf.nativeMethods.getOwnPropertyDescriptor(this.docProto, "referrer");
+    }
+}
+
+class ElementApi {
+    constructor(bf) {
+        this.bf = bf;
+        this.window = bf.window;
+        this.Element = this.window.Element;
+        this.elemProto = this.Element.prototype;
+        this.innerHTML = bf.nativeMethods.getOwnPropertyDescriptor(this.elemProto, "innerHTML");
+        this.outerHTML = bf.nativeMethods.getOwnPropertyDescriptor(this.elemProto, "outerHTML");
+    }
+}
+
+class StyleApi {
+    constructor(bf) {
+        this.bf = bf;
+        this.window = bf.window;
+        this.CSSStyleDeclaration = this.window.CSSStyleDeclaration;
+        this.cssStyleProto = this.CSSStyleDeclaration.prototype;
+        this.getPropertyValue = this.cssStyleProto.getPropertyValue;
+        this.setProperty = this.cssStyleProto.setProperty;
+    }
+}
