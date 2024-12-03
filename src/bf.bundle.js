b22310ec0bc5bda3af8066cc4bc117370aed4226
Imperialgfx <impserv178@gmail.com>
2024-12-03 15:37:52 -0800

    Update bf.bundle.js
    add the actual class

--- src/bf.bundle.js
+++ src/bf.bundle.js
@@ -1,3 +1,5 @@
+const parse5 = require('parse5');
+
 class BlissFlyBundle {
     constructor(ctx) {
         this.ctx = ctx;
@@ -6,15 +8,92 @@ class BlissFlyBundle {
         this.js = new JSRewriter(ctx);
     }
 
-    rewriteHtml(html, baseUrl) {
-        return this.html.rewrite(html, { ...this.ctx.meta, url: baseUrl });
+    async rewriteHtml(html, baseUrl) {
+        const document = parse5.parse(html);
+        const rewrittenHtml = await this.html.rewrite(document, baseUrl);
+        return parse5.serialize(rewrittenHtml);
     }
 
     rewriteCss(css, baseUrl) {
-        return this.css.rewrite(css, { ...this.ctx.meta, url: baseUrl });
+        return this.css.rewrite(css, baseUrl);
     }
 
     rewriteJs(js) {
-        return this.js.rewrite(js, this.ctx.meta);
+        return this.js.rewrite(js);
     }
 }
+
+class HTMLRewriter {
+    constructor(ctx) {
+        this.ctx = ctx;
+    }
+
+    async rewrite(document, baseUrl) {
+        await this.rewriteNode(document, baseUrl);
+        return document;
+    }
+
+    async rewriteNode(node, baseUrl) {
+        if (node.tagName) {
+            await this.rewriteElement(node, baseUrl);
+        }
+        if (node.childNodes) {
+            for (const child of node.childNodes) {
+                await this.rewriteNode(child, baseUrl);
+            }
+        }
+    }
+
+    async rewriteElement(element, baseUrl) {
+        if (!element.attrs) return;
+
+        for (const attr of element.attrs) {
+            if (this.shouldRewriteAttr(attr.name)) {
+                attr.value = this.ctx.rewriteUrl(new URL(attr.value, baseUrl).href);
+            }
+        }
+
+        if (element.tagName === 'script') {
+            await this.rewriteScript(element);
+        }
+    }
+
+    shouldRewriteAttr(attr) {
+        return ['src', 'href', 'action'].includes(attr);
+    }
+
+    async rewriteScript(element) {
+        if (element.childNodes && element.childNodes[0]) {
+            const content = element.childNodes[0].value;
+            element.childNodes[0].value = this.ctx.js.rewrite(content);
+        }
+    }
+}
+
+class CSSRewriter {
+    constructor(ctx) {
+        this.ctx = ctx;
+    }
+
+    rewrite(css, baseUrl) {
+        return css.replace(/url\(['"]?(.*?)['"]?\)/g, (match, url) => {
+            if (!url.startsWith('data:')) {
+                const absoluteUrl = new URL(url, baseUrl).href;
+                return `url("${this.ctx.rewriteUrl(absoluteUrl)}")`;
+            }
+            return match;
+        });
+    }
+}
+
+class JSRewriter {
+    constructor(ctx) {
+        this.ctx = ctx;
+    }
+
+    rewrite(js) {
+        return js;
+    }
+}
+
+module.exports = { BlissFlyBundle, HTMLRewriter, CSSRewriter, JSRewriter };
